c0d379d914368b3bd1ad577d787b6a73856ff288
Dmitrii Beliaev <dmitriymay97@gmail.com>
2023-11-16 13:57:19 +0300

    5 коммит. Написал контроллер и пустой метод сервиса Комментарии

---
+++ src/main/java/ru/skypro/homework/controller/CommentsController.java
@@ -0,0 +1,46 @@
+package ru.skypro.homework.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.comments.Comments;
+import ru.skypro.homework.dto.comments.CreateOrUpdateComment;
+import ru.skypro.homework.service.CommentsService;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/ads")
+public class CommentsController {
+
+    private final CommentsService commentsService;
+
+    public CommentsController(CommentsService commentsService) {
+        this.commentsService = commentsService;
+    }
+
+    @GetMapping("{id}/comments")
+    public Comments getComments(@RequestParam int id) {
+        return commentsService.getComments(id);
+    }
+
+    @PostMapping("{id}/comments")
+    public CreateOrUpdateComment addComment(@RequestParam int id,
+                                            @RequestBody CreateOrUpdateComment text) {
+        return commentsService.addComment(text);
+    }
+
+    @DeleteMapping("{adId}/comments/{commentId}")
+    public ResponseEntity deleteComment(@RequestParam int adId, @RequestParam int commentId) {
+        commentsService.deleteComment(adId, commentId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("{adId}/comments/{commentId}")
+    public CreateOrUpdateComment updateComment(@RequestParam int adId,
+                                               @RequestParam int commentId,
+                                               @RequestBody CreateOrUpdateComment text) {
+        return commentsService.updateComment(adId, commentId, text);
+    }
+
+}
